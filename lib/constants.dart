51c7e1d3cd5ef9ebff2240f1de4efaceb57a6fa0
Fairo Mahaputranda Faisal <112526922+Aeternaciel@users.noreply.github.com>
2023-06-22 11:25:32 +0700

    Delete lib directory

--- lib/constants.dart
+++
@@ -1,4 +0,0 @@
-const int kThemeColor = 0xffcd042e;
-const String kWelcomePageAppDescription =
-    'UPJ Digital Canteen merupakan aplikasi yang dibangun untuk menciptakan suasanan dan rasa ketertiban para Civitas UPJ dalam bertransaksi di kantin.';
-const String kWelcomeText = 'Welcome to UPJ Digital Canteen';
